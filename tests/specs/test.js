83420153ba10b14e4148a94e831335a88ede5ee1
marko <marko.sa@exodus.io>
2025-05-10 02:40:12 +0700

    Add cookie with sameSite set to none

--- tests/specs/test.js
+++ tests/specs/test.js
@@ -7,16 +7,29 @@ describe("Testing with BStackDemo", () => {
       "Title didn't match with BrowserStack"
     );
 
+    await browser.setCookies([
+      {
+        name: "user",
+        value: "1234567890",
+        path: "/",
+        secure: true,
+        httpOnly: false, // required "false" to work
+        sameSite: "none", // required "none" to work, Lax is default in chrome
+      },
+    ]);
+
     const productOnScreen = await $('//*[@id="1"]/p');
     const productOnScreenText = await productOnScreen.getText();
 
     const addToCart = await $('//*[@id="1"]/div[4]');
     await addToCart.click();
 
-    const productInCart = await $('//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]');
+    const productInCart = await $(
+      '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'
+    );
 
-    await browser.waitUntil(async () => (
-      await productInCart.getText()).match(productOnScreenText), 
+    await browser.waitUntil(
+      async () => (await productInCart.getText()).match(productOnScreenText),
       { timeout: 5000 }
     );
   });
